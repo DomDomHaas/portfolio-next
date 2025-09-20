8b7fdbc25a93dceedf46d77aa6bfc581642d9c5f
haas <dominik.haas@wsl.ch>
2025-09-20 21:54:30 +0200

    feat: hover profile picture

--- src/components/profile-image.tsx
+++ src/components/profile-image.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 
 
 export default function ProfileImage() {
+
+  const [hover, setHover] = useState(false);
+
   return(
 
 /*
@@ -10,16 +14,61 @@ export default function ProfileImage() {
   hover:border-black
 */
 
-    <>
-      <img
-        className="z-10 h-full
-                   rounded-[105px]
-                   transition-all
-                   p-0
-                   aspect-square
-                   object-cover object-center"
-        src="/images/dominikhaasartho_small.webp"
-        alt="Portrait of Dominik Haas Artho"/>
-    </>
+    <div 
+      className="z-10 h-full
+                  relative
+                  justify-self-center
+                  "
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <picture
+          style={{
+            opacity: hover ? 1 : 0
+          }}
+      >
+        <source
+          type="image/webp"
+          srcSet="/images/dominikhaasartho_p_smile.webp"
+        />
+
+        <img
+          className="h-full
+                    absolute
+                    rounded-[105px]
+                    transition-all
+                    p-0
+                    aspect-square
+                    object-cover object-top"
+          src="/images/dominikhaasartho_p_smile.jpg"
+          alt="Portrait of Dominik Haas Artho smiling"
+        />
+
+      </picture>
+
+      <picture
+          style={{
+            opacity: hover ? 0 : 1
+          }}
+      >
+        <source
+          type="image/webp"
+          srcSet="/images/dominikhaasartho_p.webp"
+        />
+
+        <img
+          className="h-full
+                    rounded-[105px]
+                    transition-all
+                    p-0
+                    aspect-square
+                    object-cover object-top"
+          src="/images/dominikhaasartho_p.jpg"
+          alt="Portrait of Dominik Haas Artho"
+        />
+
+      </picture>
+
+    </div>
   )
 }
